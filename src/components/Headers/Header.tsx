3524795e9cd277e55a6992c0369bae7fb0a45fe2
Akinro Destined <104671422+SirDest@users.noreply.github.com>
2024-09-19 14:02:30 +0100

    headers

--- src/components/Headers/Header.tsx
+++ src/components/Headers/Header.tsx
@@ -1,30 +1,29 @@
-import { Button } from "@mui/material";
 import { IoMenuOutline, IoPersonOutline, IoSearch } from "react-icons/io5";
+import Navigation from "./Navigation";
+import Subscribe from "../Subscribe";
+import SubscribeButton from "./SubscribeButton";
 
 const Header = () => {
   return (
     <>
-      <div className='flex justify-between bg-[#A0D6FA] w-full h-fit p-4'>
-        <button className='flex flex-1 items-center text-left justify-start text-[25px]'>
-          <IoMenuOutline />
-        </button>
+      <div className='flex justify-between bg-[#A0D6FA] w-full h-fit p-4 gap-4'>
+        <div className='flex md:flex-1 items-center text-left justify-start text-[25px]'>
+          <button>
+            <IoMenuOutline />
+          </button>
+        </div>
 
-        <div className='m-auto font-bold text-black text-[30px]'>
+        <div className='md:m-auto flex-1 md:flex-none justify-start font-bold text-black text-[30px]'>
           <a href='/'>Clever</a>
         </div>
 
         <div className='flex flex-1 justify-end gap-4 items-center text-right'>
-          <Button
-            className='!bg-black !text-white !text-[12px] !rounded-none'
-            size='small'
-            variant='contained'
-          >
-            SUBSCRIBE
-          </Button>
-
-          <button className='group flex gap-3 items-center hover:bg-white rounded-none cursor-pointer px-3 py-1 transition-all lg:border-l-0 border-l border-l-white'>
+          <SubscribeButton />
+          <button className='group flex gap-3 items-center hover:bg-white rounded-none cursor-pointer px-3 py-1 transition-all duration-300 lg:border-l-0 border-l border-l-white'>
             <IoPersonOutline />
-            <button className='group-hover:underline'>Sign In</button>
+            <button className='group-hover:underline hidden md:block'>
+              Sign In
+            </button>
           </button>
 
           <button className='hidden lg:block'>
@@ -32,6 +31,8 @@ const Header = () => {
           </button>
         </div>
       </div>
+      <Navigation />
+      <Subscribe />
     </>
   );
 };
--- src/components/Headers/Navigation.tsx
+++ src/components/Headers/Navigation.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
+const navItems = [
+  "Decoration",
+  "Renovation",
+  "Conversation",
+  "Shopping",
+  "Find a Pro",
+];
 const Navigation = () => {
   return (
-    <div>Navigation</div>
-  )
-}
+    <ul className='hidden lg:flex justify-center gap-4 bg-[#A0D6FA] w-full border border-white h-fit p-2 text-black'>
+      {navItems.map((items, i) => {
+        return (
+          <li className='cursor-pointer font-normal' key={i}>
+            {items}
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
-export default Navigation
+export default Navigation;
