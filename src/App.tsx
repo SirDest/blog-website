b43c35b2829acb9e929e98053056cd37d52b8c5f
Akinro Destined <104671422+SirDest@users.noreply.github.com>
2024-09-23 09:16:25 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Headers/Header";
+import Landing from "./components/Main/Landing";
+import SubscribeAd from "./components/Main/SubscribeAd";
 
 function App() {
   return (
     <div className='w-full flex flex-col'>
       <Header />
+      <SubscribeAd />
+      <Landing />
     </div>
   );
 }
---
+++ src/components/Main/Landing.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div className='h-screen w-full flex justify-center px-16 py-3'>
+      <div className='h-full w-[25%] bg-green-500'></div>
+      <div className='h-full w-[50%] bg-blue-500'></div>
+      <div className='h-full w-[25%] bg-red-500'></div>
+    </div>
+  );
+};
+
+export default Landing;
